8d2f1238165947ad07e2529ab5035c2935de6949
Snehal Chauhan <snehalbenchauhan17@gnu.ac.in>
2019-04-11 20:40:11 +0530

    updated
    updated

---
+++ WindowsFormsApplication6/sql/SQLQuery1.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE [dbo].[roombooking] (
+    [PID]       INT        NOT NULL,
+    [RoomType]  VARCHAR (10) NOT NULL,
+    [Roomno]    INT        NOT NULL,
+    [Price]     INT        NOT NULL,
+    [Status]    VARCHAR (10) NOT NULL,
+    [StartDate] VARCHAR (20) NOT NULL,
+    [EndDate]   VARCHAR (20) NOT NULL,
+    CONSTRAINT [PK_roombooking] PRIMARY KEY CLUSTERED ([PID] ASC)
+);
+
+
+
+CREATE TABLE [dbo].[PatientInfo] (
+    [pid]     INT          NOT NULL,
+    [date]    VARCHAR (20) NULL,
+    [name]    VARCHAR (20) NULL,
+    [gender]  VARCHAR (10) NULL,
+    [age]     INT          NULL,
+    [address] VARCHAR (50) NULL,
+    [disease] VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([pid] ASC)
+);
+
+
+
+CREATE TABLE [dbo].[BillPayment] (
+    [PID]             INT NOT NULL,
+    [RoomRent]        INT NOT NULL,
+    [medicineCharges] INT NOT NULL,
+    [doctorCharges]   INT NOT NULL,
+    [reportCharges]   INT NOT NULL,
+    [otherCharges]    INT NOT NULL,
+    [total]           INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([PID] ASC)
+);
